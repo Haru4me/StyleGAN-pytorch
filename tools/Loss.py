6a44b92ecdc779501bf1c4b86789a2ca16cf62bb
Haru4me <durdaev2010@yandex.ru>
2021-04-21 20:07:40 +0300

    Loss commit

--- tools/Loss.py
+++ tools/Loss.py
@@ -40,3 +40,47 @@ class WGANGP_D(nn.Module):
 
         return loss + penalty
 
+
+class MSE_G(nn.Module):
+
+    def __init__(self, reduction='mean'):
+        super(MSE_G, self).__init__()
+        self.reduction = reduction
+
+    def forward(self, preds):
+        return F.mse_loss(preds, torch.ones(preds.size(), dtype=preds.dtype,
+                                            device=preds.device), reduction=self.reduction)
+
+
+class MSE_D(nn.Module):
+
+    def __init__(self, lam=10.0, eps=0.001, penalty_target=1.0,
+                 penalty_type='grad', reduction='mean'):
+        super(MSE_D, self).__init__()
+
+        self.lam = lam
+        self.eps = eps
+        self.penalty_type = penalty_type
+        self.penalty_target = penalty_target
+        self.reduction = reduction
+
+    def forward(self, real, gen, img):
+
+        loss = 0.5*(F.mse_loss(real, torch.ones(real.size(), dtype=real.dtype, device=real.device),
+                               reduction=self.reduction) +
+                    F.mse_loss(gen, torch.zeros(gen.size(), dtype=gen.dtype, device=gen.device),
+                               reduction=self.reduction))
+
+        if self.penalty_type == 'grad':
+            grad_real = grad(outputs=real.sum(),
+                             inputs=img, create_graph=True)[0]
+            penalty = (grad_real.view(grad_real.size(0),
+                                      -1).norm(2, dim=1) ** 2).mean()
+            penalty *= self.lam / self.penalty_target**2
+            loss += penalty
+
+        elif self.penalty_type == 'eps':
+            penalty = self.eps * (real ** 2).mean()
+            loss += penalty
+
+        return loss
